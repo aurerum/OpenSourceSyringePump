4408c60bef3aa24b2e110c535fd73ce6a28b0c8b
RUM <80455782+aurerum@users.noreply.github.com>
2021-07-20 17:50:17 +0200

    Update pousse_seringue.ino

--- pousse_seringue.ino
+++ pousse_seringue.ino
@@ -1,4 +1,4 @@
-/*Example sketch to control a stepper motor with DRV8825 stepper motor driver, AccelStepper library and Arduino: continuous rotation. More info: https://www.makerguides.com */
+
 
 // Include the AccelStepper library:
 #include <AccelStepper.h>
@@ -22,7 +22,7 @@ float distance = 0.16; //Distance traveled for each injection in millimeters
 //----------------------------------------------------------------------------
 
 int cycle = timeCycle * 600;
-float stepQuantity = (distance * 3200)/8; // On multiplie la distance voulue par le nombre de pas par tour division par le pas de ma vis
+float stepQuantity = (distance * 3200)/8; 
 
 
 bool fd1Val;
@@ -66,10 +66,10 @@ void loop() {
   //Manual mode
 
   //Forward
-  if (BPavanceVal == HIGH)//Si on appuie sur le bouton avance
+  if (BPavanceVal == HIGH)
     {
 
-      while(BPavanceVal == HIGH && fd2Val == HIGH)// tant que l'on reste appuyer et qu'il n'y a pas le fin de course le moteur tourne
+      while(BPavanceVal == HIGH && fd2Val == HIGH)
       {
         digitalWrite(enable, LOW);
         stepper.setSpeed(1000);
@@ -108,11 +108,11 @@ void loop() {
       while (fd2Val == HIGH && BPreculeVal == LOW && BPavanceVal == LOW)
       {
 
-        for (int i=0 ; i<=300 ; i++)//i nombre d'injection d'eau, une toutes les 10 minutes couvre la nuit de 8h
+        for (int i=0 ; i<=300 ; i++)
         {
           readInput();
           stepper.setCurrentPosition(0);
-          while(stepper.currentPosition() != stepQuantity && fd2Val == HIGH && BPreculeVal == LOW && BPavanceVal == LOW )//On avance de 1/4 de tour
+          while(stepper.currentPosition() != stepQuantity && fd2Val == HIGH && BPreculeVal == LOW && BPavanceVal == LOW )
             {
               digitalWrite(enable, LOW);
               stepper.setSpeed(40);
@@ -126,7 +126,7 @@ void loop() {
             digitalWrite(enable, HIGH);
             
 
-            for (int j=0 ; j<=cycle ;j++)// On vérifie toutes les 100ms si le fdc ou un bouton n'est pas appuyé, j x delay = temps d'attente entre chaque injection
+            for (int j=0 ; j<=cycle ;j++)
             {
               readInput();
               if (fd2Val == LOW || BPreculeVal == HIGH || BPavanceVal == HIGH)
